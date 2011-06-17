514eb82db5300187e04663f6e518787bfb88cfd8
Ales Zoulek <ales.zoulek@gmail.com>
2011-06-17 17:29:51 +0200

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+
+
+VERSION = (0, 1, 0)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+long_description = ''
+try:
+    f = open(path.join(path.dirname(__file__), 'README.rst'))
+    long_description = f.read().strip()
+    f.close()
+except:
+    pass
+
+
+setup(
+    name = 'supervisor-wildcards',
+    description = "implemenents start/stop/restart commands with wildcard support Supervisor",
+    url = "http://github.com/aleszoulek/supervisor-wildcards",
+    long_description = long_description,
+    version = __versionstr__,
+    author = "Ales Zoulek",
+    author_email = "ales.zoulek@gmail.com",
+    license = "BSD",
+    packages = ['supervisorwildcards'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+    ]
+)
+
+
